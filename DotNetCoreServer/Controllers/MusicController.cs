b8210fed9fd39c7a37075fc881575f099d46a161
XuLezuo <xulezuo@hotmail.com>
2020-06-20 16:41:06 +0800

    update

--- DotNetCoreServer/Controllers/MusicController.cs
+++ DotNetCoreServer/Controllers/MusicController.cs
@@ -4,8 +4,10 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using DotNetCoreServer.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace DotNetCoreServer.Controllers
 {
@@ -41,18 +43,66 @@ namespace DotNetCoreServer.Controllers
         }
 
 
-        [HttpGet("GetAllPlayer", Name = "")]
-        public object GetAllPlayer()
+        [HttpGet("GetMusics/like={like}", Name = "")]
+        public object GetMusics(string like)
         {
+            var db = SugarContext.GetInstance();
+
+            var ilike = db.Queryable<I_LIKE>().ToList();
+            var map = new Dictionary<string, string>();
+            ilike.ForEach(x => map[x.MUSIC_NAME] = x.MUSIC_NAME);
             var musicer = new List<string>();
             var dic = Directory.GetFiles("../mp3");
+            var musics = new List<dynamic>();
             foreach(var ds in dic)
             {
                 var info = FileVersionInfo.GetVersionInfo(ds);
-                musicer.Add(info.ProductName);
+                var lastof = info.FileName.LastIndexOf("\\") + 1;
+                var musicname = info.FileName.Substring(lastof, info.FileName.Length - lastof);
+                var color = map.ContainsKey(musicname) ? "red" : "black";
+                dynamic dy = new
+                {
+                    name = musicname,
+                    color = color
+                };
+                if(like == "Y" && color == "red")
+                    musics.Add(dy);
+                else if(like != "Y")
+                {
+                    musics.Add(dy);
+                }
             }
-            return musicer;
+
+
+            return musics;
         }
-        
+
+
+
+
+        [HttpPost("AddILike")]
+        public void AddILike([FromBody] Object obj)
+        {
+            var db = SugarContext.GetInstance();
+            var ilike = JsonConvert.DeserializeObject<I_LIKE>(Convert.ToString(obj));
+            var isexist = db.Queryable<I_LIKE>().Where(x => x.MUSIC_NAME == ilike.MUSIC_NAME).ToList();
+            if(isexist == null || isexist.Count == 0)
+            {
+                var info = new I_LIKE
+                {
+                    ID = Guid.NewGuid().ToString("N").ToUpper(),
+                    USER_CREATED = ilike.USER_CODE,
+                    USER_CODE = ilike.USER_CODE,
+                    DATETIME_CREATED = DateTime.Now,
+                    MUSIC_NAME = ilike.MUSIC_NAME,
+                    STATE = "A"
+                };
+                db.Insertable(info).ExecuteCommand();
+            }
+            else
+            {
+                db.Deleteable<I_LIKE>().Where(x=>x.ID == isexist[0].ID).ExecuteCommand();
+            }
+        }
     }
 }
---
+++ DotNetCoreServer/Models/I_LIKE.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetCoreServer.Models
+{
+    internal partial class I_LIKE
+    {
+        public string ID { get; set; }
+        public DateTime DATETIME_CREATED { get; set; }
+        public string USER_CREATED { get; set; }
+        public DateTime? DATETIME_MODIFIED { get; set; }
+        public string USER_MODIFIED { get; set; }
+        public string STATE { get; set; }
+        public string MUSIC_NAME { get; set; }
+        public string USER_CODE { get; set; }
+    }
+}
